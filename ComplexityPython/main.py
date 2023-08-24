8b396745384802c41c638c6169483d2bbfaa4b1b
arqFabian <72966763+arqFabian@users.noreply.github.com>
2023-08-24 12:02:57 +0900

    Corrections to HistoryArchStylesTheory.tex to add the art noaveu refrence as well as the ransition to modernism

--- ComplexityPython/main.py
+++ ComplexityPython/main.py
@@ -2,25 +2,20 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import cv2
-import numpy as np
 import os
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 from ComplexityAnalysis import calculate_complexity
 
-input_folder = 'C:/Users/arqfa/Downloads/complexity'  # Replace with the folder path
-output_folder = 'C:/Users/arqfa/Downloads/complexity'  # Replace with the folder where you want to save results
+# Get the directory of the script
+script_directory = os.path.dirname(__file__)
+
+# Construct the input folder path using the project folder as root
+
+input_folder = os.path.join(script_directory, 'input_images')# Replace with the folder path
+output_folder = os.path.join(script_directory, 'input_images') # Replace with the folder where you want to save results
+
+# Initialize a list to store results
+results = []
 
 # Iterate through all images in the input folder
 for filename in os.listdir(input_folder):
@@ -31,10 +26,9 @@ for filename in os.listdir(input_folder):
         # Create a result string
         result_string = f"{filename} complexity score: {complexity_score}"
 
-        # Print the result
-        print(result_string)
+        # Add the result to the results list
+        results.append(result_string)
 
-        # Save the result to a text file in the output folder
-        result_file_path = os.path.join(output_folder, f"{filename.split('.')[0]}_result.txt")
-        with open(result_file_path, 'w') as result_file:
-            result_file.write(result_string)
+# Print all results
+for result in results:
+    print(result)
