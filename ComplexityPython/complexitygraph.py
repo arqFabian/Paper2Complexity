445ffb000a40c28d4f091f447efa9c7fe89e5749
arqFabian <72966763+arqFabian@users.noreply.github.com>
2023-09-20 16:29:29 +0900

    Arrangement of images for complexity analysis into folders and adaptation of scatter graph for it.

--- ComplexityPython/complexitygraph.py
+++ ComplexityPython/complexitygraph.py
@@ -2,36 +2,56 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import cycle
 
 
-def generate_scatter_complexity(file_names_input, complexity_scores_input, script_directory_input, trendline_degree_input):
+def generate_scatter_complexity(years_input, complexity_scores_input, style_labels_input, style_names_input, script_directory_input, trendline_input):
     # Set the desired aspect ratio (16:9)
     aspect_ratio = 12 / 4
 
     # Calculate the figure width based on the aspect ratio
-    fig_width = 10  # You can adjust this value as needed
+    fig_width = 12  # You can adjust this value as needed
 
     # Calculate the corresponding figure height
     fig_height = fig_width / aspect_ratio
 
+    # Create a colormap for styles
+    style_cmap = plt.get_cmap('tab20')  # Change 'tab20' to the desired colormap name
+
+    # Assign colors based on style from the colormap
+    style_colors = {style: style_cmap(i) for i, style in enumerate(style_names_input)}
+
+    # Map the colors to the style labels
+    point_colors = [style_colors[style] for style in style_labels_input]
+
+    # Create a scatter plot
     # Set the figure size with the desired aspect ratio
     plt.figure(figsize=(fig_width, fig_height))
+    plt.scatter(years_input, complexity_scores_input, c=point_colors, label=style_labels_input) # Add label to create legend
 
-    # Create a scatter plot with polynomial trend line
-    plt.scatter(file_names_input, complexity_scores_input, marker='o', color='b')
+    # Set the legend to the right of the graph
+    handles = [plt.Line2D([0], [0], marker='o', color='w', markerfacecolor=style_colors[style], markersize=10, label=style) for
+        style in style_names_input]
+
+    # Add the trendline to the legend with a label
+    handles.append(plt.Line2D([0], [0], color='r', linestyle='--', label='Trend Line'))
+
+    plt.legend(handles=handles, title='Styles', loc='center left', bbox_to_anchor=(1, 0.5))  # Position legend to the right
+
+    #label axis
     plt.xlabel('Chronological order of buildings')
     plt.ylabel('Complexity Score')
-    plt.title('Facade Complexity Analysis')
+    plt.title('Facade Complexity Analysis by Year and Style')
     plt.grid(True)
 
     # Fit a polynomial trend line
 
-    trend_coefficients = np.polyfit(file_names_input, complexity_scores_input, trendline_degree_input) # Change this to the degree of the polynomial (e.g., 2 for quadratic)
-    trend_line = np.polyval(trend_coefficients, file_names_input)
-    plt.plot(file_names_input, trend_line, color='r', linestyle='--', label='Trend Line')
+    trend_coefficients = np.polyfit(years_input, complexity_scores_input, trendline_input) # Change this to the degree of the polynomial (e.g., 2 for quadratic)
+    trend_line = np.polyval(trend_coefficients, years_input)
+    plt.plot(years_input, trend_line, color='r', linestyle='--')
 
-    # Show the scatter plot with polynomial trend line
-    plt.legend()
+    # Use tight_layout to ensure proper layout and fit legend
+    plt.tight_layout()
 
     # Specify the relative path to the Graphs directory from the script directory
     relative_graphs_path = os.path.join('..', 'src', 'Graphs')
@@ -40,10 +60,11 @@ def generate_scatter_complexity(file_names_input, complexity_scores_input, scrip
     save_path = os.path.join(script_directory_input, relative_graphs_path, 'complexitygraph.pdf')
 
     # Adjust figure margins to remove extra space
-    plt.subplots_adjust(left=0.09, right=0.98, top=0.9, bottom=0.2)
+    #plt.subplots_adjust(left=0.09, right=0.98, top=0.9, bottom=0.2)
 
     # Save the plot as a PDF file
-    plt.savefig(save_path)
+    plt.savefig(save_path, bbox_inches='tight')
     plt.show()
 
     return
+
--- ComplexityPython/main.py
+++ ComplexityPython/main.py
@@ -4,37 +4,44 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import cycle
 
-from ComplexityAnalysis import calculate_complexity
+from complexityanalysis import calculate_complexity
 from complexitygraph import generate_scatter_complexity
 
 # Get the directory of the script
 script_directory = os.path.dirname(__file__)
 
-# Construct the input folder path using the project folder as root
+# Define styles and extract the style names from folder names
+input_folder = os.path.join(script_directory, 'input_images')
+style_names = [folder for folder in os.listdir(input_folder) if os.path.isdir(os.path.join(input_folder, folder))]
 
-input_folder = os.path.join(script_directory, 'input_images')# Replace with the folder path
-output_folder = os.path.join(script_directory, 'input_images') # Replace with the folder where you want to save results
+# Create a colormap for styles
+style_cmap = plt.get_cmap('tab20')  # Change 'tab20' to the desired colormap name
 
-# Initialize lists to store file names and complexity scores
-file_names = []
+# Initialize lists to store data
+file_paths = []
 complexity_scores = []
+style_labels = []
+years = []
 
-# Iterate through all images in the input folder
-for filename in os.listdir(input_folder):
-    if filename.lower().endswith((".png", ".jpg")):
-        """if filename.endswith(".png"):"""
-        image_path = os.path.join(input_folder, filename)
-        complexity_score = calculate_complexity(image_path)
-
-        # Append file name (number) and complexity score to lists
-        file_names.append(int(filename.split('.')[0]))
-        complexity_scores.append(complexity_score)
+# Iterate through style folders and images
+for style in style_names:
+    style_path = os.path.join(input_folder, style)
+    for filename in os.listdir(style_path):
+        if filename.lower().endswith((".png", ".jpg")):
+            image_path = os.path.join(style_path, filename)
+            complexity = calculate_complexity(image_path)
+            year = int(filename.split('_')[0])  # Extract the year from the filename
+            file_paths.append(image_path)
+            complexity_scores.append(complexity)
+            style_labels.append(style)
+            years.append(year)
 
 # Print all results
 for result in complexity_scores:
     print(result)
 
 # create the scatter graph using the function for scatter graph
-generate_scatter_complexity(file_names, complexity_scores, script_directory, 8)
+generate_scatter_complexity(years, complexity_scores, style_labels, style_names, script_directory, 8)
 
