53ae14a21877989c669274d7d28e55ec4dc2f598
arqFabian <72966763+arqFabian@users.noreply.github.com>
2023-10-10 14:53:31 +0900

    Creation of raph function for patterns and blender script for fbx blenderfbx.py

--- ComplexityPython/blenderfbx.py
+++ ComplexityPython/blenderfbx.py
@@ -2,7 +2,68 @@ import bpy
 import os
 
 
-def export_collection_to_fbx(collection_name, folder_name):
+def export_collection_to_fbx(collection_name, folder_name, frame):
+    # Get the directory where the current .blend file is saved
+    blend_file_directory = os.path.dirname(bpy.data.filepath)
+
+    # Ensure the Blender file is saved
+    if not blend_file_directory:
+        raise Exception("Please save the Blender file before running the script.")
+
+    # Set the path where the exported files will be saved
+    export_path = os.path.join(blend_file_directory, folder_name)
+
+    # Ensure the path exists
+    os.makedirs(export_path, exist_ok=True)
+
+    # Deselect all objects
+    bpy.ops.object.select_all(action='DESELECT')
+
+    # Set the frame to export
+    bpy.context.scene.frame_set(frame)
+
+    # Reference the collection
+    try:
+        collection = bpy.data.collections[collection_name]
+    except KeyError:
+        print(f"No collection found with name {collection_name}")
+        collection = None
+
+    # Export each object in the collection
+    if collection:
+        for obj in collection.objects:
+            # Ensure the object is a mesh
+            if obj.type == 'MESH':
+                # Select the object
+                obj.select_set(True)
+                bpy.context.view_layer.objects.active = obj
+
+                # Set the export path and filename
+                file_path = os.path.join(export_path, f"{obj.name}.fbx")
+
+                # Export the object as FBX without baking animations
+                bpy.ops.export_scene.fbx(
+                    filepath=file_path,
+                    use_selection=True,
+                    bake_anim=False  # Disable baking animations
+                )
+
+                # Deselect the object after exporting
+                obj.select_set(False)
+
+
+# Apply function to export fbx for each collection and frame 12 which is the point where all facades are in place.
+
+# P1
+# export_collection_to_fbx("P1", "fbxModels\P1", 12)
+
+# P2
+# export_collection_to_fbx("P2", "fbxModels\P2", 12)
+
+# P3
+# export_collection_to_fbx("P3", "fbxModels\P3", 12)
+
+def export_collection_to_fbx1(collection_name, folder_name):
     # Get the directory where the current .blend file is saved
     blend_file_directory = os.path.dirname(bpy.data.filepath)
 
@@ -48,8 +109,11 @@ def export_collection_to_fbx(collection_name, folder_name):
                 # Deselect the object after exporting
                 obj.select_set(False)
 
-# export_collection_to_fbx("P1", "UnityModels\P1")
+# P1
+# export_collection_to_fbx("P1", "fbxModels\P1")
 
-# export_collection_to_fbx("P2", "UnityModels\P2")
+# P2
+# export_collection_to_fbx("P2", "fbxModels\P2")
 
- export_collection_to_fbx("P3", "UnityModels\P3")
+# P3
+# export_collection_to_fbx("P3", "fbxModels\P3")
