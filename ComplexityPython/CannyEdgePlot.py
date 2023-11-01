2bf92fe73ed3d29e4d8f2cc71bda5ff36e152a26
arqFabian <72966763+arqFabian@users.noreply.github.com>
2023-11-01 11:59:55 +0900

    Alteration to the script of the edge canny plot so we can see all the images

--- ComplexityPython/CannyEdgePlot.py
+++ ComplexityPython/CannyEdgePlot.py
@@ -3,7 +3,8 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 import os
 
-def process_image(image_path):
+# Working function for side by side comparison of Original and Edge detection image
+def process_image1(image_path):
     # Read the image from the specified file path
     img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
 
@@ -37,10 +38,72 @@ input_folder = os.path.join(script_directory, 'input_images')
 filename = '1954_Notre-Dame-du-Haut-Ronchamp-France.jpg'
 image_path = os.path.join(input_folder, filename)
 
-process_image(image_path)
+process_image1(image_path)
 
-input_folder = os.path.join(script_directory, 'render_images\Pattern 1')
+input_folder = os.path.join(script_directory, 'render_images\Temp')
 filename = '0010.png' # Replace with the path to your image
 image_path = os.path.join(input_folder, filename)
 
-process_image(image_path)
+process_image1(image_path)
+
+
+# Working Plot of all edge detection Images
+
+def process_images2(image_paths):
+    # Calculate the number of rows and columns for the grid
+    num_images = len(image_paths)
+    num_cols = 4  # You can change the number of columns as needed
+    num_rows = (num_images + num_cols - 1) // num_cols
+
+    # Create a figure with subplots
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(12, 8))
+
+    # Flatten the axes array for easy iteration
+    axes = axes.flatten()
+
+    for i, image_path in enumerate(image_paths):
+        # Read the image from the specified file path
+        img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+
+        # Check if the image was successfully read
+        if img is None:
+            raise Exception(f"File '{image_path}' could not be read. Please check if the file exists.")
+
+        # Apply Canny edge detection
+        edges = cv.Canny(img, 100, 200)
+
+        """# Display the original image in the current subplot
+        axes[i].imshow(img, cmap='gray')
+        axes[i].set_title(f'Image {i+1}')
+        axes[i].axis('off')  # Turn off axis labels"""
+
+        # Display the edge detection image in the second subplot
+        axes[i].imshow(edges, cmap='gray')
+        axes[i].set_title(f'Edge Detection Lv {i+1}')
+        axes[i].axis('off')  # Turn off axis labels
+
+    # Use tight_layout to ensure proper layout
+    plt.tight_layout()
+
+    # Show the figure
+    plt.show()
+
+
+
+# Example usage:
+# Get the directory of the script
+script_directory = os.path.dirname(__file__)
+
+# Define the folder containing images
+input_folder = os.path.join(script_directory, r'render_images\Temp')
+
+# Create a list of image file paths in the input folder
+image_paths = [os.path.join(input_folder, filename) for filename in os.listdir(input_folder) if filename.lower().endswith((".png", ".jpg", ".jpeg", ".webp"))]
+
+# Process and display all images in a single plot window
+process_images2(image_paths)
+
+
+
+
+
